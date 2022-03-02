dcd45b48599f2719acbdb39b3ca7831c81e9bc55
muhuiyu <muhuiyu.grace@gmail.com>
2022-03-02 08:07:40 +0800

    Initial Commit

---
+++ MuFitnessGal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MuFitnessGal
+//
+//  Created by Mu Yu on 3/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
